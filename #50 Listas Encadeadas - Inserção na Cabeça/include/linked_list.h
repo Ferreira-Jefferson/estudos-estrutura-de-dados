8af39d53e396716b0bb5828872b00c496a7edc40
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-05-28 05:21:07 +0000

    #50 Listas Encadeadas - Inserção na Cabeça

---
+++ #50 Listas Encadeadas - Inserção na Cabeça/include/linked_list.h
@@ -0,0 +1,11 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+typedef struct _snode SNode;
+typedef struct _linked_list LinkedList;
+
+SNode *SNode_create();
+LinkedList *LinkedList_create();
+void LinkedList_add_first(LinkedList* L, int val);
+
+#endif
